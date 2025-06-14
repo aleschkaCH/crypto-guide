9305578038eeb004e688b0cab7f999afa5aad67c
Алексей <140729324+aleschkaCH@users.noreply.github.com>
2025-06-14 17:33:45 +0300

    dev

---
+++ config.js
@@ -0,0 +1,14 @@
+//  /config.js
+
+const CONFIG = {
+    siteName: 'База знаний криптовалют',
+    version: '1.0.0',
+    author: 'Crypto Guide CH',
+    social: {
+        telegram: '@cryptoguide',
+        twitter: '@cryptoguide'
+    },
+    analytics: {
+        // Google Analytics, Yandex.Metrica
+    }
+};
